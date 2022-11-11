898ee17386b14dca09c615bf13542e900f779356
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-11 10:29:48 +0100

    Create Header component and embed it into App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Hello World</h1>
+    <Header />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Title</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderElement',
+};
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content:space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
